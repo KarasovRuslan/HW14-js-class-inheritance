3953777d69358a923521088db17763b47d0fb66e
KarasovRuslan <karasov.ruslan@icloud.com>
2021-12-22 16:57:47 +0200

    HW14 commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let clockTime = document.getElementById('clockTime');
+
+function Clock(e) {
+    this.clockTime = e;
+    this.isFullFormat = true;
+};
+
+function AssemblyClock(e) {
+    Clock.call(this, e);
+    
+    Clock.prototype.render = function () {
+        let date = new Date();
+        this.hours = (date.getHours() < 10) ? '0' + date.getHours() : date.getHours(); 
+        this.minutes = (date.getMinutes() < 10) ? '0' + date.getMinutes() : date.getMinutes();  
+        this.seconds = (date.getSeconds() < 10) ? '0' + date.getSeconds() : date.getSeconds();
+
+        this.clockTime.innerText = this.isFullFormat ? + this.hours + ":" + this.minutes + ":" + this.seconds : this.hours + ":" + this.minutes;
+    };
+
+    this.switchFormat = function() {
+        this.isFullFormat = !this.isFullFormat;
+    };
+};
+
+AssemblyClock.prototype = this.Clock.prototype;
+let assembly = new AssemblyClock(clockTime);
+
+clockTime.addEventListener('click', (e) => {
+    if(e.target.tagName === 'DIV') {
+        assembly.switchFormat();
+    }
+});
+
+setInterval(() => {
+    assembly.render() 
+}, 250);
